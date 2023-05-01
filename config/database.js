7a8eb7afb2f62ea4f77f2fd5cd5b21774206f551
Maimounadev <maimounatraoredev@gmail.com>
2023-04-30 23:47:45 -0400

    savage - auth

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://maimounatraoredev:Enai123@cluster0.ikhc6zs.mongodb.net/savageauth?retryWrites=true&w=majority',
+    'dbName': 'savAuth'
+};
